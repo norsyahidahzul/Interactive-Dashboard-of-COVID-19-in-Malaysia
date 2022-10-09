1bd948742fc24e354c7695377c33386dda090e0f
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-09 21:14:35 +0800

    Update 1_📈_COVID19_Simulator.py

--- pages/1_📈_COVID19_Simulator.py
+++ pages/1_📈_COVID19_Simulator.py
@@ -12,13 +12,14 @@ def COVID19_Simulator():
     )
 
  if model == 'Classical':
+   st.slider("Infection rate" (st.latex(\beta)), min_value=0, max_value=100, value=3, step=0.1)
     st.write('**You have selected Classical SEIRD model simulator :smile:.**')
     st.subheader("**Classical SEIRD model simulator**")
   
     if st.button('Click here for more info on its mathematical expressions'):
          st.write('The mathematical expression of classical SEIRD model in terms of ordinary differential equations (ODE) can be expressed as follows:')
-         st.latex(r'''\frac{ds}{dt} = -\frac{\beta_IS\left(t\right)I\left(t\right)}{N}''')
-         st.latex(r'''\frac{dE}{dt} = \frac{\beta_IS\left(t\right)I\left(t\right)}{N} ''')
+         st.latex(r'''\frac{ds}{dt} = -\frac{\beta S\left(t\right)I\left(t\right)}{N}''')
+         st.latex(r'''\frac{dE}{dt} = \frac{\beta S\left(t\right)I\left(t\right)}{N} ''')
          st.latex(r'''\frac{dI}{dt} = \sigma E\left(t\right) - \gamma I\left(t\right)- \mu I\left(t\right)''')
          st.latex(r'''\frac{dR}{dt} =  \gamma I\left(t\right)''')
          st.latex(r'''\frac{dD}{dt} =  \delta R\left(t\right)''')
@@ -32,9 +33,9 @@ def COVID19_Simulator():
          st.write('The mathematical expression of modified SEIRD model in terms of ordinary differential equations (ODE) can be expressed as follows:')
          st.latex(r'''\frac{ds}{dt} = -\frac{\beta_I\left(t\right)S\left(t\right)I\left(t\right)}{N} -\frac{\beta_E\left(t\right)S\left(t\right)E\left(t\right)}{N} 
          + \delta\left(t\right)R\left(t\right)''')
-          st.latex(r'''\frac{dE}{dt} = \frac{\beta_I\left(t\right)S\left(t\right)I\left(t\right)}{N} +\frac{\beta_E\left(t\right)S\left(t\right)E\left(t\right)}{N} 
+         st.latex(r'''\frac{dE}{dt} = \frac{\beta_I\left(t\right)S\left(t\right)I\left(t\right)}{N} +\frac{\beta_E\left(t\right)S\left(t\right)E\left(t\right)}{N} 
           - \sigma\left(t\right)E\left(t\right)''')
-          st.latex(r'''\frac{dI}{dt} = \sigma\left(t\right)E\left(t\right) - \gamma\left(t\right)I\left(t\right)- \mu\left(t\right)I\left(t\right)''')
+         st.latex(r'''\frac{dI}{dt} = \sigma\left(t\right)E\left(t\right) - \gamma\left(t\right)I\left(t\right)- \mu\left(t\right)I\left(t\right)''')
          st.latex(r'''\frac{dR}{dt} =  \gamma\left(t\right)I\left(t\right)- \delta\left(t\right)R\left(t\right)''')
          st.latex(r'''\frac{dD}{dt} =  \delta\left(t\right)R\left(t\right)''')
    
