e0d8e21e629d46f1e49edca28cada7307335a6f2
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-09 22:08:01 +0800

    Update classicalmodel.py

--- classicalmodel.py
+++ classicalmodel.py
@@ -89,8 +89,5 @@ fig.update_layout(title='Simulation of Classical SEIRD Model',
                      )
 fig.update_xaxes(tickangle=0, tickformat = None ,tickmode='array', tickvals=np.arange(0, days + 1,step))
 
-
-fig.write_image("images/Cseird_simulation_withoutHD_non-optimised_constantparams.png")
-fig.show()
 st.pyplot(fig)
 
