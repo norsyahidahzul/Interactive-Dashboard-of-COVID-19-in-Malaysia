22a069eb941a72fee8b66964f5316f067f490b7e
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-12 03:02:36 +0800

    Update 3_📊_Overview_of_Cases.py

--- pages/3_📊_Overview_of_Cases.py
+++ pages/3_📊_Overview_of_Cases.py
@@ -8,10 +8,7 @@ def Overview_of_Cases():
     st.set_page_config(page_title="Overview of Cases", page_icon="📊")
     st.markdown("# Overview of Cases")
     st.sidebar.header("Overview of Cases")
-    st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-    (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-    )
+    
 
     st.write(
     """This page gives you an overview of COVID-19 cases in Malaysia during first,second and early of third waves based on general cases or state cases. 
