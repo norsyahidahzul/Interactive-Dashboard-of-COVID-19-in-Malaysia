2f3a99c8705970d5c4c58816cee17ee355e28fc8
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2022-10-12 02:56:37 +0800

    Update 2_🌍_COVID19_Predictor.py

--- pages/2_🌍_COVID19_Predictor.py
+++ pages/2_🌍_COVID19_Predictor.py
@@ -4,20 +4,19 @@ import streamlit as st
 
 def COVID19_Predictor():
   
-st.set_page_config(page_title="COVID-19 Predictor", page_icon="🌍")
-st.markdown("# COVID-19 Predictor")
-st.sidebar.header("COVID-19 Predictor")
-st.write(
+  st.set_page_config(page_title="COVID-19 Predictor", page_icon="🌍")
+  st.markdown("# COVID-19 Predictor")
+  st.sidebar.header("COVID-19 Predictor")
+  st.write(
     """COVID-19 Predictor gives you insight on the effective or successfullness of movement control order implemented by government 
-    and population behaviours.
-"""
-)
+    and population behaviours.  
+  """
+  )
 
-st.write(
+  st.write(
     """**This page is under development. Please come again later.**
-"""
-)
-
+  """
+  )
 
 COVID19_Predictor()
 
