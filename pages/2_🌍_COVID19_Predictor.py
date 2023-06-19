70a6a64c6b66d5a90b696e978649f05484e59a6c
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2023-06-19 19:22:40 +0800

    Update 2_🌍_COVID19_Predictor.py

--- pages/2_🌍_COVID19_Predictor.py
+++ pages/2_🌍_COVID19_Predictor.py
@@ -40,7 +40,7 @@ def COVID19_Predictor():
  
  # Read in data from the Google Sheet.
  # Uses st.cache_data to only rerun when the query changes or after 10 min.
- @st.cache_data(ttl=600)
+ #@st.cache_data(ttl=600)
  def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
